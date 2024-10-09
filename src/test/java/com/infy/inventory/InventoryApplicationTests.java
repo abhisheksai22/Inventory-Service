fefa54d7cf2fe2510fac4e111e46ee60b6b43a63
Abhishek <70491019+abhisheksai22@users.noreply.github.com>
2024-10-09 23:36:03 +0530

    Push

---
+++ src/test/java/com/infy/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
